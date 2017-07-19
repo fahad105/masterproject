648d8dea1e84079d945db62d04b8f64d91aa202d
fahad105 <m.f.ali1995@gmail.com>
2017-07-19 23:00:12 +0200

    Update curltest.sh

--- kasadaka/curltest.sh
+++ kasadaka/curltest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+#Note: This file is not used in the final version of the DonkeyBox
 /usr/bin/curl -F "upfile=@/etc/network/localdatastore.txt" 192.168.77.1:8080 >> /etc/network/curllog.txt
 echo "uploaded file ----------" >> /etc/network/connectionlog.txt
 /usr/bin/curl 192.168.77.1/Shared/syncedfile.txt >> /etc/network/remotedatastore.txt
